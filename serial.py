dfeea4be2a328918b8bbaf62bb12304359b7fd9d
Jordan Wong <pjwong97@gmail.com>
2022-07-26 15:22:10 -0700

    complete serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self,start = 0):
+        self.start = start
+        self.count = start
+
+    def __repr__(self):
+        return f'I am currently at {self.count}'
+
+    def generate(self):
+        self.count = self.count + 1
+        return self.count - 1
+
+    def reset(self):
+        self.count = self.start
