63e8ded1f040c2abb86a1a687b6308d6b6a963d5
Jordan Wong <pjwong97@gmail.com>
2022-07-26 15:55:08 -0700

    complete wordfinder class

--- serial.py
+++ serial.py
@@ -18,15 +18,23 @@ class SerialGenerator:
     100
     """
     def __init__(self,start = 0):
+        """constructor create instance to set start and count"""
+
         self.start = start
         self.count = start
 
     def __repr__(self):
+        """return instance with current count"""
+
         return f'I am currently at {self.count}'
 
     def generate(self):
+        """returns current number and adds 1"""
+
         self.count = self.count + 1
         return self.count - 1
 
     def reset(self):
+        """resets current count to start count"""
+
         self.count = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,21 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self,path):
+        self.path = path
+        self.word_list = self.word_list()
+        print (f'{len(self.word_list)} words read')
+
+    def __repr__(self):
+        return f'Word Finder path = {self.path}'
+
+    def word_list(self):
+        file = open(self.path)
+        return [line.strip() for line in file]
+
+    def random(self):
+        return choice(self.word_list)
+
+
